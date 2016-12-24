3b39afa5598f0942d67f289852c23d90178ff7df
pouletalunizado <dvivb@openmailbox.org>
2016-12-24 10:11:04 +0100

    Add files via upload

---
+++ primero.py
@@ -0,0 +1,19 @@
+70*33
+333*222
+print ("hola mundo")
+class miprimeraclase:
+	pass
+a = miprimeraclase()
+b = miprimeraclase()
+print(a)
+print(b)
+class Punto:
+	pass
+p1 = Punto()
+p2 = Punto()
+p1.x = 5
+p1.y = 6
+p2.x = 4
+p2.y = 3
+print(p1.x, p1.y)
+
