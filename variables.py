54b01151665089be5d48c847297f3259c91126cf
pouletalunizado <dvivb@openmailbox.org>
2016-12-26 15:29:45 +0100

    Add files via upload
    definición de variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+
+print ("hola mundo")
+hola mundo
+num_entero = 10
+num_real=13.1415
+tipo_cadena = "hola"
+tipo_bool = True
+print (num_entero)
+print (num_entero)
+print (num_real)
+print (tipo_cadena)
+resultado_suma = 10+15
+print(resultado_suma)
