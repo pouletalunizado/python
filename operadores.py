0dee2151f6b6f1a82db4100269526be51f7fb51b
pouletalunizado <dvivb@openmailbox.org>
2016-12-26 17:20:41 +0100

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,18 @@
+
+#operadores logicos
+sumar = 2 + 3 + 4 +6
+restar =100 -3 -4 -5 
+multiplicar= 23 *3
+dividir = 20 / 3
+resto = 27 % 3
+exponente = 2 ** 2
+print (sumar)
+print (restar)
+print (multiplicar)
+print (resto)
+print (exponente)
+print (dividir)
+base = 30
+altura = 50
+superficie = base * altura / 2
+print (superficie)
