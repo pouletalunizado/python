d5dc9b15977922c4ef249c53a73bf79391fdc9aa
pouletalunizado <dvivb@openmailbox.org>
2016-12-31 16:55:07 +0100

    Add files via upload

---
+++ thinkpython.py
@@ -0,0 +1,39 @@
+message = 'hola'
+n = 17
+pi = 3.15
+print (message)
+type (message)
+minute =59
+minute/60
+miles = 26.2
+miles * 1.61
+first = 'throat'
+second = 'warbler'
+print (first + second)
+width =17
+height =12.0
+delimiter = '.'
+width/2
+width/2.0
+height/3
+1 + 2 * 5
+delimiter * 5
+int ('32')
+int (3.45)
+int (-3.3)
+float(32)
+float('3.14')
+str(3.24)
+import math
+print (math)
+degrees = 45
+radians  = degrees /360 * 2 * math.pi
+math.sin(radians)
+math.sqrt (2) / 2.0
+x = math.sin(degrees/ 360 )
+x = math.exp(math.log(x+1))
+print (x)
+def print_lyrics():
+ 	print ("I am lumberjack")
+def repeat_lyrics():
+	print_lyrics()
